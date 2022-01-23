02ee7f699a0e72ff3cb779d23d142f5af5987e26
HexaTools <98209392+hexatools@users.noreply.github.com>
2022-01-23 16:47:00 +0800

    Delete caseconverter.js

--- caseconverter.js
+++
@@ -1,6 +0,0 @@
-const sidebar = document.getElementsByClassName(".sidebar");
-const toggle = document.getElementsByClassName(".toggle");
-
-    toggle.addEventListener("click", () =>{
-    sidebar.classList.toggle("close");
-    });
